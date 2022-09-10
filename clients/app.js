7bc9f5a6fa51097b89ad5b529eee45be54a2f3dc
Peter T <p25tran@gmail.com>
2022-09-09 21:01:40 -0400

    first commit

---
+++ clients/app.js
@@ -0,0 +1,50 @@
+const tableBody = document.getElementById('table-body')
+
+const getFlight = () => {
+    fetch('http://localhost:3000/flights')
+        .then(response => response.json())
+        .then(flights =>{
+            console.log(flights)
+            populateTable(flights)
+
+        })
+        .catch(err => console.log(err))
+}
+
+getFlight();
+
+const populateTable = (flights) => {
+    for ( const flight of flights) {
+    const tableRow = document.createElement('tr')
+    const tableIcon = document.createElement('td')
+    tableIcon.textContent = "✈︎"
+    tableRow.append(tableIcon)
+
+    const flightDetails={
+        time:flight.departing.slice(0,5),
+        destination:flight.destination.toUpperCase(),
+        flight:flight.flightNumber.shift(),
+        gate:flight.gate,
+        remarks:flight.status.toUpperCase()
+    }
+
+    for (const flightDetail in flightDetails){
+        const tableCell = document.createElement('td')
+        const word = Array.from(flightDetails[flightDetail])
+
+        for(const [index,letter] of word.entries()){
+            const letterElement = document.createElement('div')
+
+            setTimeout(() => {
+                letterElement.classList.add('flip')
+                letterElement.textContent = letter
+                letterElement.textcontent = letter; }, 100 * index)  // setting interval for each letter to flip 
+
+            letterElement.textContent = letter
+            tableCell.append(letterElement)
+        }
+        tableRow.append(tableCell)
+    }
+    tableBody.append(tableRow)
+    }
+}
